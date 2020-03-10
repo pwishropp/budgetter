49c422a191132d4c3c4b9bdfa57ae854a60b6f0c
Paul Wishropp <pwishropp@gmail.com>
2020-03-09 21:33:53 -0500

    Added links to navbar/form submission

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,6 +1,24 @@
 class BudgetsController < ApplicationController
 
     def index
+      @budgets = Budget.all
+    end
+
+    def new
+      @budget = Budget.new
+    end
+
+    def create
+      Budget.create(budget_params)
+      redirect_to root_path
+    end
+
+    private
+
+    def budget_params
+      params.require(:budget).permit(:month, :income, :extra_income, :actual_personal_savings,
+                                     :actual_tfsa, :actual_rrsp
+  )
     end
 
 end
